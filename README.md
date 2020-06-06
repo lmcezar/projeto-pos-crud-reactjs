803285669a259505dfc52e9f21805fbfb92f6114
Daniel Cavalcante <danielcdesouza@gmail.com>
2020-06-05 21:02:20 -0300

    Close mvp

--- README.md
+++ README.md
@@ -10,6 +10,12 @@
 
 ### https://github.com/jquense/yup
 
+### https://sweetalert2.github.io/
+
+### https://github.com/sweetalert2/sweetalert2-react-content
+
 ## Outros links
+
+### https://www.netlify.com/
 
 ### https://nestjs.com/
--- src/pages/List.js
+++ src/pages/List.js
@@ -1,12 +1,15 @@
 import React, { useEffect, useState } from "react";
 import { Container, Row, Button, Table } from "react-bootstrap";
-import axios from "axios";
+import withReactContent from "sweetalert2-react-content";
+import Swal from "sweetalert2";
 import moment from "moment";
+import axios from "axios";
 
 const url = "https://node-todo-dev.herokuapp.com/api/todos";
 
 export default (props) => {
   const [todos, setTodos] = useState([]);
+  const MySwal = withReactContent(Swal);
   useEffect(() => {
     findTodos();
   }, []);
@@ -17,12 +20,33 @@ export default (props) => {
     });
   }
 
+  function handleRemove() {
+    MySwal.fire({
+      title: "Você tem certeza?",
+      text: "Uma vez deletado, você não poderá recuperar este TODO.",
+      type: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#d9534f",
+      confirmButtonText: "Sim",
+      cancelButtonText: "Não",
+    }).then((isConfirmed) => {
+      if (isConfirmed.value) {
+        // axios.delete(`${url}/${id}`).then((res) => {});
+      }
+    });
+  }
+
   function mapTodos() {
     return todos.map((todo) => (
       <tr>
         <td>{todo.description}</td>
         <td>{moment(todo.createdAt).format("DD/MM/YYYY")}</td>
         <td>{todo.done ? "Sim" : "Não"}</td>
+        <td>
+          <Button variant="danger" onClick={handleRemove}>
+            Remover
+          </Button>
+        </td>
       </tr>
     ));
   }
@@ -47,6 +71,7 @@ export default (props) => {
               <th>Descrição</th>
               <th>Data de criação</th>
               <th>Concluído</th>
+              <th>Ação</th>
             </tr>
           </thead>
           <tbody>{mapTodos()}</tbody>
